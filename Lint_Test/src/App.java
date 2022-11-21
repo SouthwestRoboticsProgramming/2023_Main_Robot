26316d0ee57f52e8fa8513f8f14a190b6315ae24
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-11-21 09:51:20 -0600

    Example bad java code for testing

---
+++ Lint_Test/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+                   System.out.println("This is bad style on soooooooooooooooo many levels");
+    }
+}
