5a7f42b538d42f0ae7ba5a0a5cdefa56a28eb212
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-11-22 13:03:49 -0600

    Add example test

---
+++ Robot/src/test/java/com/swrobotics/lib/ExampleTest.java
@@ -0,0 +1,11 @@
+package com.swrobotics.lib;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ExampleTest {
+    @Test
+    public void succeed() {
+        assertTrue(true);
+    }
+}
