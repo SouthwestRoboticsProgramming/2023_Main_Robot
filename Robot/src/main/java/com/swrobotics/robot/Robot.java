a66b3f0f03c5c7492d813d7735b77836655a74c3
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-11-25 17:20:18 -0600

    [Robot] Add messenger test

--- Robot/src/main/java/com/swrobotics/robot/Robot.java
+++ Robot/src/main/java/com/swrobotics/robot/Robot.java
@@ -1,6 +1,9 @@
 package com.swrobotics.robot;
 
+import com.swrobotics.lib.schedule.Scheduler;
+import com.swrobotics.lib.schedule.Subsystem;
 import com.swrobotics.lib.wpilib.AbstractRobot;
+import com.swrobotics.messenger.client.MessengerClient;
 
 public final class Robot extends AbstractRobot {
     private static final double PERIODIC_PER_SECOND = 50;
@@ -10,5 +13,21 @@ public final class Robot extends AbstractRobot {
     }
     
     @Override
-    protected final void addSubsystems() {}
+    protected final void addSubsystems() {
+        MessengerClient msg = new MessengerClient("10.21.29.3", 5805, "RIO");
+
+        Scheduler.get().addSubsystem(new Subsystem() {
+            @Override
+            public void periodic() {
+                msg.prepare("Something")
+                        .addDouble(Math.random())
+                        .addInt(83)
+                        .send();
+
+                msg.readMessages();
+
+                System.out.println("Messenger: " + msg.isConnected());
+            }
+        });
+    }
 }
