ea6c75aa03c25c87a7f2a0e3e359f6ba0f151cdf
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-11-27 21:46:51 -0600

    Add CANCoder using Encoder framework

---
+++ Robot/src/main/java/com/swrobotics/lib/encoder/CANCoderEncoder.java
@@ -0,0 +1,67 @@
+package com.swrobotics.lib.encoder;
+
+import com.ctre.phoenix.sensors.AbsoluteSensorRange;
+import com.ctre.phoenix.sensors.CANCoder;
+import com.ctre.phoenix.sensors.CANCoderConfiguration;
+import com.ctre.phoenix.sensors.CANCoderStatusFrame;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
+import com.ctre.phoenix.sensors.SensorTimeBase;
+import com.swrobotics.mathlib.Angle;
+import com.swrobotics.mathlib.CWAngle;
+
+public class CANCoderEncoder extends Encoder{
+    private final static int TIMEOUT_MS = 100;
+
+    private final CANCoder encoder;
+
+    /**
+     * Creates a new instance using a specified CAN ID. This constructor
+     * assumes the CANCoder is connected to the RoboRIO CAN bus.
+     * 
+     * @param id CAN ID of the CANCoder
+     */
+    public CANCoderEncoder(int id) {
+        encoder = new CANCoder(id);
+        configEncoder(encoder);
+    }
+
+    /**
+     * Creates a new instance using a specified CAN ID and bus. The default
+     * CAN bus is named {@code "rio"}, as it is the RoboRIO integrated CAN bus.
+     * A different CAN bus can be specified to, for example, identify a
+     * CANCoder attached through a CANivore.
+     * 
+     * @param id CAN ID of the CANCoder
+     * @param canbus CAN bus the CANCoder is wired to
+     */
+    public CANCoderEncoder(int id, String canbus) {
+        encoder = new CANCoder(id, canbus);
+        configEncoder(encoder);
+    }
+    
+    // Sets the CTRE configuration for the CANCoder
+    private void configEncoder(CANCoder encoder) {
+        encoder.configFactoryDefault(TIMEOUT_MS);
+    
+        CANCoderConfiguration config = new CANCoderConfiguration();
+        {
+            config.absoluteSensorRange = AbsoluteSensorRange.Unsigned_0_to_360;
+            config.initializationStrategy = SensorInitializationStrategy.BootToAbsolutePosition;
+            config.sensorTimeBase = SensorTimeBase.PerSecond;
+            config.magnetOffsetDegrees = 0;
+        }
+    
+        encoder.configAllSettings(config, TIMEOUT_MS);
+        encoder.setStatusFramePeriod(CANCoderStatusFrame.SensorData, 1);
+    }
+
+    @Override
+    public Angle getRawAngleImpl() {
+        return CWAngle.deg(encoder.getAbsolutePosition());
+    }
+
+    @Override
+    public Angle getVelocityImpl() {
+        return CWAngle.deg(encoder.getVelocity());
+    }
+}
