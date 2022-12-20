09723fe0a59cdc8ba6bf97b20b4019c3fbf24eec
Ryan Heuer <63077980+rmheuer@users.noreply.github.com>
2022-12-20 09:40:43 -0600

    [Robot] Bring in NetworkTables library

---
+++ Robot/src/main/java/com/swrobotics/lib/net/NTDoubleArray.java
@@ -0,0 +1,33 @@
+package com.swrobotics.lib.net;
+
+/**
+ * Represents a {@code double[]} value stored in NetworkTables.
+ */
+public class NTDoubleArray extends NTEntry<double[]> {
+    private final double[] defaultVals;
+
+    public NTDoubleArray(String path, double... defaultVals) {
+        super(path, defaultVals);
+        this.defaultVals = defaultVals;
+    }
+
+    @Override
+    public double[] get() {
+        return entry.getDoubleArray(defaultVals);
+    }
+
+    public double get(int index) {
+        return get()[index];
+    }
+
+    @Override
+    public void set(double[] value) {
+        entry.setDoubleArray(value);
+    }
+
+    public void set(int index, double value) {
+        double[] data = get();
+        data[index] = value;
+        set(data);
+    }
+}
---
+++ Robot/src/main/java/com/swrobotics/lib/net/NTIntegerArray.java
@@ -0,0 +1,49 @@
+package com.swrobotics.lib.net;
+
+/**
+ * Represents a {@code int[]} value stored in NetworkTables.
+ */
+public final class NTIntegerArray extends NTEntry<int[]> {
+    private final double[] defaultVals;
+
+    public NTIntegerArray(String path, int... defaultVals) {
+        super(path, defaultVals);
+        this.defaultVals = intArrayToDoubleArray(defaultVals);
+    }
+
+    @Override
+    public int[] get() {
+        return doubleArrayToIntArray(entry.getDoubleArray(defaultVals));
+    }
+
+    public int get(int index) {
+        return get()[index];
+    }
+
+    @Override
+    public void set(int[] data) {
+        entry.setDoubleArray(intArrayToDoubleArray(data));
+    }
+
+    public void set(int index, int value) {
+        int[] data = get();
+        data[index] = value;
+        set(data);
+    }
+
+    private double[] intArrayToDoubleArray(int[] data) {
+        double[] out = new double[data.length];
+        for (int i = 0; i < data.length; i++) {
+            out[i] = data[i];
+        }
+        return out;
+    }
+
+    private int[] doubleArrayToIntArray(double[] data) {
+        int[] out = new int[data.length];
+        for (int i = 0; i < data.length; i++) {
+            out[i] = (int) data[i];
+        }
+        return out;
+    }
+}
