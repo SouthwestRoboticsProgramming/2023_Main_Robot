98da118647bd198f1f9627b931ca532f898c0d9e
rmheuer <91571536+mvog2501@users.noreply.github.com>
2022-12-27 11:05:22 -0600

    [Robot] Added SwerveModuleInfo to describe module IDs
    This currently does not include offset as that would conflict with later calibration work

---
+++ Robot/src/main/java/com/swrobotics/robot/subsystems/SwerveModuleInfo.java
@@ -0,0 +1,19 @@
+package com.swrobotics.robot.subsystems;
+
+/**
+ * Describes the IDs of a swerve module. This is used to allow swerve modules to be hot-swapped
+ */
+public class SwerveModuleInfo {
+
+    public final String name;
+    public final int driveMotorID;
+    public final int turnMotorID;
+    public final int encoderID;
+    
+    public SwerveModuleInfo(String name, int driveMotorID, int turnMotorID, int encoderID) {
+        this.name = name;
+        this.driveMotorID = driveMotorID;
+        this.turnMotorID = turnMotorID;
+        this.encoderID = encoderID;
+    }
+}
