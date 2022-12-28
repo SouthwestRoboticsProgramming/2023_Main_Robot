d2571cf381e6bc7c1eb1efed6a401ab23c576995
Museum25 <91571536+mvog2501@users.noreply.github.com>
2022-12-28 15:44:09 -0600

    [Robot] Added "reset pose" block to BlockAuto

--- Robot/src/main/java/com/swrobotics/robot/blockauto/AutoBlocks.java
+++ Robot/src/main/java/com/swrobotics/robot/blockauto/AutoBlocks.java
@@ -11,6 +11,8 @@ import com.swrobotics.robot.RobotContainer;
 import com.swrobotics.robot.blockauto.part.AnglePart.Mode;
 import com.swrobotics.robot.subsystems.Lights;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -19,6 +21,7 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 import com.swrobotics.mathlib.Angle;
+import com.swrobotics.mathlib.Vec2d;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -116,6 +119,36 @@ public final class AutoBlocks {
                 .text("Robot relative: ")
                 // .paramBoolean(false)
                 .creator((params, robot) -> new TurnToAngleCommand(robot, (Angle) params[0], false));
+    
+        drive.newBlock("reset pose")
+            .text("Reset pose to ")
+            .paramVec2d(0.0, 0.0)
+            .text("(wpi)")
+            .paramAngle(Mode.CW_DEG, 0.0)
+            .text("(cw deg)")
+            .creator((params, robot) -> new CommandBase() {
+                @Override
+                public void initialize() {
+                    System.out.println("Resetting pose");
+                    
+                    Vec2d translation = (Vec2d) params[0];
+                    Angle rotation = (Angle) params[1];
+
+                    Pose2d newPose = new Pose2d(
+                        new Translation2d(translation.x, translation.y),
+                        rotation.cw().rotation2d()
+                    );
+
+                    // Reset gyro before reseting odometry to fix field oriented drive
+                    robot.m_drivetrainSubsystem.setGyroscopeRotation(newPose.getRotation());
+                    robot.m_drivetrainSubsystem.resetPose(newPose);
+                }
+
+                @Override
+                public boolean isFinished() {
+                    return true;
+                }
+            });
         
         initRegistryAndValidate();
     }
