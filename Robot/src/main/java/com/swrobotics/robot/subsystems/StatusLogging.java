2e6299eadc0f1bc25dd5059fe22b930105812076
foehammer127 <foehammer127@gmail.com>
2022-12-28 15:09:50 -0600

    Renamed to Status Logging

--- Robot/src/main/java/com/swrobotics/robot/subsystems/StatusLogging.java
+++ Robot/src/main/java/com/swrobotics/robot/subsystems/StatusLogging.java
@@ -5,9 +5,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.HashMap;
 
 
-public class ErrorLogging extends SubsystemBase {
+public class StatusLogging extends SubsystemBase {
     private int queuelen = 0;
-   public HashMap<Integer, RobotError> Errors;
+   public HashMap<Integer, RobotError> Statuses;
 
    /* Adds an Error To the Error Log Data Structure, and returns an id value that the throwing subsystem can retain,
     so it can later resolve it.
