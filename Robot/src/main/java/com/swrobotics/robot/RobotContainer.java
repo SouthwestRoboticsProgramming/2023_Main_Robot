03e57ca9e15282e1a1380edbd0295ede2e1fb70b
rmheuer <91571536+mvog2501@users.noreply.github.com>
2022-12-27 20:47:22 -0600

    [Robot] Increase deadband for neutral mode to take effect

--- Robot/src/main/java/com/swrobotics/robot/RobotContainer.java
+++ Robot/src/main/java/com/swrobotics/robot/RobotContainer.java
@@ -222,7 +222,7 @@ public class RobotContainer {
 
     private static double modifyAxis(double value) {
         // Deadband
-        value = deadband(value, 0.05);
+        value = deadband(value, 0.15);
 
         // Square the axis
         value = Math.copySign(value * value, value);
