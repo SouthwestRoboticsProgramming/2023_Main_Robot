95b70dc8605492cfff74026d26a9e5a7823bb658
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-01-05 19:32:47 -0600

    [ShuffleLog] Delete Rapid React field vector layer, 2022 season is over

--- ShuffleLog/src/main/java/com/swrobotics/shufflelog/tool/field/FieldViewTool.java
+++ ShuffleLog/src/main/java/com/swrobotics/shufflelog/tool/field/FieldViewTool.java
@@ -64,7 +64,6 @@ public final class FieldViewTool extends ViewportTool {
         MessengerClient msg = log.getMessenger();
         layers = new ArrayList<>();
         layers.add(new MeterGridLayer());
-        layers.add(new FieldVectorLayer());
         layers.add(new PathfindingLayer(msg, this));
         layers.add(new TagTrackerLayer(this, msg));
         layers.add(new WaypointLayer(this, msg));
