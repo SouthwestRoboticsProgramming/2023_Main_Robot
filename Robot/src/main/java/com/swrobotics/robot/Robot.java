8864643da466cbf26ca4a1392c8f0dd53a16d587
mvog2501 <mvog2501@mpsedu.org>
2023-01-08 22:26:38 -0600

    [Robot] Removed unused import

--- Robot/src/main/java/com/swrobotics/robot/Robot.java
+++ Robot/src/main/java/com/swrobotics/robot/Robot.java
@@ -3,7 +3,6 @@ package com.swrobotics.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
