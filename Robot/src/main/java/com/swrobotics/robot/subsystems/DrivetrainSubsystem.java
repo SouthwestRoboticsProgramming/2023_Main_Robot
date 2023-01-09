1dcbf31c0a044350d66c2226b1b6d5e27e4f0b93
mvog2501 <mvog2501@mpsedu.org>
2023-01-09 11:09:16 -0600

    [Robot] Removed unused drive import

--- Robot/src/main/java/com/swrobotics/robot/subsystems/DrivetrainSubsystem.java
+++ Robot/src/main/java/com/swrobotics/robot/subsystems/DrivetrainSubsystem.java
@@ -21,7 +21,6 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
