00a02757a45e90a772f36bbae2d22726d770ca01
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-01-27 10:09:53 -0600

    [ButtonPanel] Test light matrix

--- ButtonPanel/ButtonPanel.ino
+++ ButtonPanel/ButtonPanel.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+#define SELECT_PORT PORTB
+#define SELECT_DDR DDRB
+#define LIGHT_PORT PORTD
+#define LIGHT_DDR DDRD
 
+#define MATRIX_GROUP_COUNT 6
+#define MATRIX_GROUP_SIZE 6
+
+#define PERSIST_TIME 3
+
+void setup() {
+  uint8_t size_mask = (2 << MATRIX_GROUP_SIZE) - 1;
+  SELECT_DDR = size_mask;
+  LIGHT_DDR = size_mask;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (uint8_t group = 0; group < MATRIX_GROUP_COUNT; group++) {
+    SELECT_PORT = 1 << group;
+    LIGHT_PORT = 0; // All lights on
 
+    delay(PERSIST_TIME);
+  }
 }
