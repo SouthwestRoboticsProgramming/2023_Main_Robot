885d2d4cf3a30ab187abad3a8edcdf7831d3f1bc
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-01-27 12:30:04 -0600

    [ButtonPanel] Send button state over serial

--- ButtonPanel/ButtonPanel.ino
+++ ButtonPanel/ButtonPanel.ino
@@ -11,12 +11,14 @@
 //#define PERSIST_TIME 250
 #define PERSIST_TIME 2.5
 
-// Converts from button inputs on BUTTON_PIN to lights on LIGHT_PORT
+#define PACKET_BEGIN 0xA5
+uint8_t packet_buf[MATRIX_GROUP_COUNT + 1];
+
 // Since Teensy does not have pins F2 and F3 exposed, we need to shift
 // the bits around to account for it
 // B | 1 2 . . 3 4 5 6
 // L | 1 2 3 4 5 6
-inline uint8_t buttons_to_lights(uint8_t buttons_group) {
+inline uint8_t shift_down_buttons(uint8_t buttons_group) {
   return (buttons_group & 3) | ((buttons_group & 0xF0) >> 2);
 }
 
@@ -29,13 +31,25 @@ void setup() {
 
   // Set as input
   BUTTON_DDR = 0;
+
+  Serial.begin(115200);
+  packet_buf[0] = PACKET_BEGIN;
 }
 
+// All code here must run as fast as possible to avoid flickering lights
 void loop() {
   for (uint8_t group = 0; group < MATRIX_GROUP_COUNT; group++) {
     SELECT_PORT = 1 << group;
     delayMicroseconds(20); // Give the select pins some time to switch
-    LIGHT_PORT = buttons_to_lights(BUTTON_PIN); // Lights on if button isn't pressed
+    
+    uint8_t buttons_group = shift_down_buttons(BUTTON_PIN);
+    LIGHT_PORT = buttons_group; // Lights on if button isn't pressed
+
+    packet_buf[group + 1] = buttons_group;
+
     delay(PERSIST_TIME);
   }
+
+  // Dump button state over serial, will be decoded by ShuffleLog
+  Serial.write(packet_buf, sizeof(packet_buf));
 }
