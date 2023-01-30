efa6378b3b4f2bc3e8faae60dc1561cca204d344
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-01-30 11:43:17 -0600

    [ShuffleLog] Add virtual button panel

--- ShuffleLog/src/main/java/com/swrobotics/shufflelog/tool/buttons/ButtonPanelTool.java
+++ ShuffleLog/src/main/java/com/swrobotics/shufflelog/tool/buttons/ButtonPanelTool.java
@@ -4,12 +4,14 @@ import com.swrobotics.shufflelog.tool.Tool;
 import imgui.ImGui;
 import imgui.flag.ImGuiCol;
 import imgui.flag.ImGuiColorEditFlags;
+import imgui.flag.ImGuiStyleVar;
 
 public final class ButtonPanelTool implements Tool {
     private final ButtonPanel panel;
 
     public ButtonPanelTool() {
-        panel = new SerialButtonPanel();
+//        panel = new SerialButtonPanel();
+        panel = new VirtualButtonPanel();
     }
 
     private void showGUI() {
@@ -21,18 +23,20 @@ public final class ButtonPanelTool implements Tool {
             }
 
             ImGui.separator();
+            ImGui.pushStyleVar(ImGuiStyleVar.ItemSpacing, 0, 0);
             for (int row = 0; row < 4; row++) {
                 for (int col = 0; col < 9; col++) {
                     float on = panel.isButtonDown(col, row) ? 1 : 0;
                     ImGui.pushStyleColor(ImGuiCol.Border, ImGui.colorConvertFloat4ToU32(on, on, on, 1));
 
                     ImGui.colorButton("##" + row + "," + col, new float[] {on, on, on, 1}, ImGuiColorEditFlags.NoTooltip);
-                    ImGui.sameLine(0, 0);
+                    ImGui.sameLine();
 
                     ImGui.popStyleColor();
                 }
                 ImGui.newLine();
             }
+            ImGui.popStyleVar();
         }
         ImGui.end();
     }
