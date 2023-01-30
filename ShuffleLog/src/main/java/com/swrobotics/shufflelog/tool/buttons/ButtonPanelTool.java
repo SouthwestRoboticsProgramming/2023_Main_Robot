e188af583e78c5ac05649ee507f8ed44946e1d41
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-01-30 11:16:17 -0600

    [ShuffleLog] Refactor out serial communication

--- ShuffleLog/src/main/java/com/swrobotics/shufflelog/tool/buttons/ButtonPanelTool.java
+++ ShuffleLog/src/main/java/com/swrobotics/shufflelog/tool/buttons/ButtonPanelTool.java
@@ -1,121 +1,32 @@
 package com.swrobotics.shufflelog.tool.buttons;
 
-import com.fazecast.jSerialComm.SerialPort;
 import com.swrobotics.shufflelog.tool.Tool;
 import imgui.ImGui;
 import imgui.flag.ImGuiCol;
 import imgui.flag.ImGuiColorEditFlags;
 
-import java.util.Arrays;
-
 public final class ButtonPanelTool implements Tool {
-    private static final byte START_BYTE = (byte) 0xA5;
-
-    // Buttons are numbered from left to right, then top to bottom
-    // IDs skip the NOODS button
-    private static final int[][] GROUP_ID_TO_BUTTON_MAP = {
-            {27, 18,  9, 0, 35, 17},
-            {28, 19, 10, 1, 34, 16},
-            {29, 20, 11, 2, 33, 15},
-            {30, 21, 12, 3, 26,  8},
-            {31, 22, 13, 4, 25,  7},
-            {32, 23, 14, 5, 24,  6}
-    };
-
-    private SerialPort serial;
-    private boolean waitingForStart;
-    private final byte[] readBuf = new byte[6];
-    private int readIdx;
-
-    private final boolean[] buttonStates = new boolean[36];
-    private final byte[] currentData = new byte[6];
+    private final ButtonPanel panel;
 
     public ButtonPanelTool() {
-        serial = null;
-        waitingForStart = false;
-        readIdx = 0;
+        panel = new SerialButtonPanel();
     }
-
-    private void onButtonData() {
-        System.arraycopy(readBuf, 0, currentData, 0, 6);
-
-        for (int group = 0; group < 6; group++) {
-            for (int bit = 0; bit < 6; bit++) {
-                int buttonIdx = GROUP_ID_TO_BUTTON_MAP[group][bit];
-                buttonStates[buttonIdx] = (readBuf[group] & (1 << bit)) != 0;
-            }
-        }
-    }
-
-    private void updateLightData() {
-        byte[] data = new byte[7];
-        data[0] = START_BYTE;
-        for (int i = 1; i < 7; i++) {
-            data[i] = (byte) (Math.random() * 63);
-        }
-        serial.writeBytes(data, data.length);
-    }
-
-    private void updateButtons() {
-        if (serial == null) {
-            SerialPort[] ports = SerialPort.getCommPorts();
-            if (ports.length == 0) return;
-            serial = ports[0];
-
-            serial.openPort();
-        }
-
-        if (serial.bytesAvailable() <= 0)
-            return;
-
-        byte[] incoming = new byte[serial.bytesAvailable()];
-        int read = serial.readBytes(incoming, incoming.length);
-
-        for (int i = 0; i < read; i++) {
-            if (waitingForStart) {
-                if (incoming[i] == START_BYTE) {
-                    waitingForStart = false;
-                    readIdx = 0;
-                }
-            } else {
-                readBuf[readIdx++] = incoming[i];
-                if (readIdx == 6) {
-                    // Packet finished
-                    onButtonData();
-                    waitingForStart = true;
-                }
-            }
-        }
-    }
-
-    long lastRandomTime = System.currentTimeMillis();
 
     private void showGUI() {
         if (ImGui.begin("Button Panel")) {
-            if (serial == null) {
+            if (panel.isConnected()) {
                 ImGui.text("Not connected");
             } else {
-                ImGui.text("Connected on serial port: " + serial.getDescriptivePortName());
+                ImGui.text("Connected");
             }
-
-            long time = System.currentTimeMillis();
-            if (time - lastRandomTime > 250) {
-                updateLightData();
-                lastRandomTime = time;
-            }
-
-            ImGui.text("Button data");
-            ImGui.text(Arrays.toString(currentData));
 
             ImGui.separator();
             for (int row = 0; row < 4; row++) {
                 for (int col = 0; col < 9; col++) {
-                    int idx = col + row * 9;
-
-                    float on = buttonStates[idx] ? 1 : 0;
+                    float on = panel.isButtonDown(col, row) ? 1 : 0;
                     ImGui.pushStyleColor(ImGuiCol.Border, ImGui.colorConvertFloat4ToU32(on, on, on, 1));
 
-                    ImGui.colorButton("##" + idx, new float[] {on, on, on, 1}, ImGuiColorEditFlags.NoTooltip);
+                    ImGui.colorButton("##" + row + "," + col, new float[] {on, on, on, 1}, ImGuiColorEditFlags.NoTooltip);
                     ImGui.sameLine(0, 0);
 
                     ImGui.popStyleColor();
@@ -128,7 +39,7 @@ public final class ButtonPanelTool implements Tool {
 
     @Override
     public void process() {
-        updateButtons();
+        panel.processIO();
         showGUI();
     }
 }
---
+++ ShuffleLog/src/main/java/com/swrobotics/shufflelog/tool/buttons/SerialButtonPanel.java
@@ -0,0 +1,142 @@
+package com.swrobotics.shufflelog.tool.buttons;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.swrobotics.shufflelog.util.Cooldown;
+
+import java.util.Arrays;
+
+/**
+ * Implementation of the button panel that communicates
+ * with the physical button panel over serial.
+ */
+public final class SerialButtonPanel implements ButtonPanel {
+    private static final class Button {
+        private final int group;
+        private final byte bit;
+
+        public Button(int group, int bitIdx) {
+            this.group = group;
+            bit = (byte) (1 << bitIdx);
+        }
+    }
+
+    // Indexed BUTTONS[x][y]
+    // Mapping from button locations to data bits
+    private static final Button[][] BUTTONS = {
+            {new Button(0, 3), new Button(0, 2), new Button(0, 1), new Button(0, 0)},
+            {new Button(1, 3), new Button(1, 2), new Button(1, 1), new Button(1, 0)},
+            {new Button(2, 3), new Button(2, 2), new Button(2, 1), new Button(2, 0)},
+            {new Button(3, 3), new Button(3, 2), new Button(3, 1), new Button(3, 0)},
+            {new Button(4, 3), new Button(4, 2), new Button(4, 1), new Button(4, 0)},
+            {new Button(5, 3), new Button(5, 2), new Button(5, 1), new Button(5, 0)},
+            {new Button(5, 5), new Button(2, 5), new Button(5, 4), new Button(2, 4)},
+            {new Button(4, 5), new Button(1, 5), new Button(4, 4), new Button(1, 4)},
+            {new Button(3, 5), new Button(0, 5), new Button(3, 4), new Button(0, 4)}
+    };
+
+    private static final byte START_BYTE = (byte) 0xA5;
+    private static final int LIGHT_DATA_COOLDOWN = 100; // Milliseconds
+
+    private static final int GROUP_SIZE = 6;
+    private static final int GROUP_COUNT = 6;
+    private static final byte GROUP_MASK = (1 << GROUP_SIZE) - 1;
+
+    private SerialPort serial;
+    private boolean waitingForStart;
+    private final byte[] readBuf = new byte[GROUP_COUNT];
+    private int readIdx;
+
+    private final byte[] buttonStates = new byte[GROUP_COUNT];
+    private final byte[] lightStates = new byte[GROUP_COUNT];
+
+    private final Cooldown lightDataCooldown;
+
+    public SerialButtonPanel() {
+        Arrays.fill(buttonStates, (byte) 0);
+        Arrays.fill(lightStates, GROUP_MASK);
+
+        lightDataCooldown = new Cooldown(LIGHT_DATA_COOLDOWN);
+        serial = null;
+    }
+
+    @Override
+    public boolean isButtonDown(int x, int y) {
+        Button button = BUTTONS[x][y];
+        return (buttonStates[button.group] & button.bit) != 0;
+    }
+
+    @Override
+    public void setButtonLight(int x, int y, boolean on) {
+        Button button = BUTTONS[x][y];
+
+        // Bits are negated in lightStates
+        if (on)
+            lightStates[button.group] &= ~button.bit;
+        else
+            lightStates[button.group] |= button.bit;
+    }
+
+    @Override
+    public boolean isConnected() {
+        return serial != null;
+    }
+
+    @Override
+    public void processIO() {
+        // If not connected, try to connect
+        if (serial == null) {
+            SerialPort[] availPorts = SerialPort.getCommPorts();
+            if (availPorts.length == 0)
+                return;
+
+            serial = availPorts[0];
+            serial.openPort();
+
+            waitingForStart = true;
+            readIdx = 0;
+        }
+
+        // Write out light data
+        if (lightDataCooldown.request()) {
+            byte[] packet = new byte[GROUP_COUNT + 1];
+            packet[0] = START_BYTE;
+            System.arraycopy(lightStates, 0, packet, 1, GROUP_COUNT);
+
+            int written = 0;
+            while (written < packet.length) {
+                int attempt = serial.writeBytes(packet, packet.length - written, written);
+                if (attempt < 0) {
+                    System.err.println("Button panel disconnected due to write error");
+                    serial.closePort();
+                    serial = null;
+                    return;
+                }
+                written += attempt;
+            }
+        }
+
+        // Read in button data
+        int avail = serial.bytesAvailable();
+        if (avail <= 0)
+            return;
+
+        byte[] incoming = new byte[serial.bytesAvailable()];
+        int read = serial.readBytes(incoming, incoming.length);
+        for (int i = 0; i < read; i++) {
+            byte b = incoming[i];
+
+            if (waitingForStart) {
+                if (b == START_BYTE) {
+                    waitingForStart = false;
+                    readIdx = 0;
+                }
+            } else {
+                readBuf[readIdx++] = b;
+                if (readIdx == readBuf.length) {
+                    System.arraycopy(readBuf, 0, buttonStates, 0, GROUP_COUNT);
+                    waitingForStart = true;
+                }
+            }
+        }
+    }
+}
