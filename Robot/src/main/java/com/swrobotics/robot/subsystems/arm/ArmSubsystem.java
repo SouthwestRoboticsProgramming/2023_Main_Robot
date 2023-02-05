7f3cbbbb3441c61195ab29effebe08c82a7f3b98
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-02-04 21:48:00 -0600

    [Robot] Fix build

--- Robot/src/main/java/com/swrobotics/robot/subsystems/arm/ArmSubsystem.java
+++ Robot/src/main/java/com/swrobotics/robot/subsystems/arm/ArmSubsystem.java
@@ -157,7 +157,6 @@ public final class ArmSubsystem extends SubsystemBase {
 
                 if (dist < followTol * followTol) {
                     currentTarget = pose;
-                    status.set("following");
                     break;
                 }
             }
