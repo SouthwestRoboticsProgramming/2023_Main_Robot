e798b7da40bae0195d4f52272b3f2ee3c08a2764
mvog2501 <mvog2501@mpsedu.org>
2023-02-08 16:09:22 -0600

    [Robot] Added a fast and a slow mode to drive

--- Robot/src/main/java/com/swrobotics/robot/commands/DefaultDriveCommand.java
+++ Robot/src/main/java/com/swrobotics/robot/commands/DefaultDriveCommand.java
@@ -4,25 +4,36 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 import com.swrobotics.robot.subsystems.drive.DrivetrainSubsystem;
 
 public class DefaultDriveCommand extends CommandBase {
+    private static final double SLOW_MODE_MULTIPLIER = 0.5;
+    private static final double FAST_MODE_MULTIPLIER = 2.0;
+
     private final DrivetrainSubsystem drivetrainSubsystem;
 
     private final DoubleSupplier translationXSupplier;
     private final DoubleSupplier translationYSupplier;
     private final DoubleSupplier rotationSupplier;
 
+    private final BooleanSupplier slowModeSupplier;
+    private final BooleanSupplier fastModeSupplier;
+
     public DefaultDriveCommand(DrivetrainSubsystem drive,
                                DoubleSupplier translationXSupplier,
                                DoubleSupplier translationYSupplier,
-                               DoubleSupplier rotationSupplier) {
+                               DoubleSupplier rotationSupplier,
+                               BooleanSupplier slowModeSupplier,
+                               BooleanSupplier fastModeSupplier) {
         this.drivetrainSubsystem = drive;
         this.translationXSupplier = translationXSupplier;
         this.translationYSupplier = translationYSupplier;
         this.rotationSupplier = rotationSupplier;
+        this.slowModeSupplier = slowModeSupplier;
+        this.fastModeSupplier = fastModeSupplier;
 
         addRequirements(drive);
     }
@@ -31,11 +42,24 @@ public class DefaultDriveCommand extends CommandBase {
     public void execute() {
         // You can use `new ChassisSpeeds(...)` for robot-oriented movement instead of field-oriented movement
 
+        double multiplier = 1.0;
+
+        if (slowModeSupplier.getAsBoolean()) {
+            multiplier *= SLOW_MODE_MULTIPLIER;
+        }
+
+        if (fastModeSupplier.getAsBoolean()) {
+            multiplier *= FAST_MODE_MULTIPLIER;
+        }
+
+        double x = translationXSupplier.getAsDouble() * multiplier;
+        double y = translationYSupplier.getAsDouble() * multiplier;
+
         if (RobotBase.isSimulation()) {
             drivetrainSubsystem.setChassisSpeeds(
                 ChassisSpeeds.fromFieldRelativeSpeeds(
-                        translationXSupplier.getAsDouble(),
-                        translationYSupplier.getAsDouble(),
+                        x,
+                        y,
                         rotationSupplier.getAsDouble(),
                         drivetrainSubsystem.getPose().getRotation()
                 )
@@ -45,8 +69,8 @@ public class DefaultDriveCommand extends CommandBase {
 
         drivetrainSubsystem.setChassisSpeeds(
                 ChassisSpeeds.fromFieldRelativeSpeeds(
-                        translationXSupplier.getAsDouble(),
-                        translationYSupplier.getAsDouble(),
+                        x,
+                        y,
                         rotationSupplier.getAsDouble(),
                         drivetrainSubsystem.getGyroscopeRotation()
                 )
