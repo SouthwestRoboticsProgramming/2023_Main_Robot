6bbc1fa0158f3a2ad169113e1c69f12259ca57fa
mvog2501 <mvog2501@mpsedu.org>
2023-02-08 10:27:25 -0600

    [Robot] Added a sequence to fully balance

---
+++ Robot/src/main/java/com/swrobotics/robot/commands/BalanceSequenceCommand.java
@@ -0,0 +1,21 @@
+package com.swrobotics.robot.commands;
+
+import com.swrobotics.mathlib.Angle;
+import com.swrobotics.mathlib.CCWAngle;
+import com.swrobotics.robot.RobotContainer;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class BalanceSequenceCommand extends SequentialCommandGroup {
+    public BalanceSequenceCommand(RobotContainer robot, boolean fromOutsideCommunity) {
+        Angle angle = Angle.ZERO;
+        if (fromOutsideCommunity) {
+            angle = CCWAngle.deg(180);
+        }
+
+        addCommands(
+            new StartBalanceCommand(robot, angle, 0.75, false),
+            new AutoBalanceCommand(robot)
+        );
+    }
+}
