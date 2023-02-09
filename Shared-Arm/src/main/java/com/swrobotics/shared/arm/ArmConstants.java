9af4f6ffe6138cda6d6d89f7b61a48bd98e85416
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-02-09 13:17:50 -0600

    [Shared-Arm] Set accurate gear ratios

--- Shared-Arm/src/main/java/com/swrobotics/shared/arm/ArmConstants.java
+++ Shared-Arm/src/main/java/com/swrobotics/shared/arm/ArmConstants.java
@@ -5,9 +5,9 @@ public final class ArmConstants {
     public static final double BOTTOM_LENGTH = 1.25;
     public static final double TOP_LENGTH = 1;
 
-    // Gear ratios from motor output to arm joint movement  FIXME
+    // Gear ratios from motor output to arm joint movement
     public static final double BOTTOM_GEAR_RATIO = 600;
-    public static final double TOP_GEAR_RATIO = 300;
+    public static final double TOP_GEAR_RATIO = 288;
 
     private ArmConstants() {
         throw new AssertionError();
