f2a69915e6f0e8d6b098b7d0018de924ce9cfd94
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-02-10 14:46:19 -0600

    [Shared-Arm] Set arm lengths

--- Shared-Arm/src/main/java/com/swrobotics/shared/arm/ArmConstants.java
+++ Shared-Arm/src/main/java/com/swrobotics/shared/arm/ArmConstants.java
@@ -1,9 +1,11 @@
 package com.swrobotics.shared.arm;
 
+import edu.wpi.first.math.util.Units;
+
 public final class ArmConstants {
-    // Lengths of the arm segments in meters  FIXME
-    public static final double BOTTOM_LENGTH = 1.25;
-    public static final double TOP_LENGTH = 1;
+    // Lengths of the arm segments in meters
+    public static final double BOTTOM_LENGTH = Units.inchesToMeters(38.5);
+    public static final double TOP_LENGTH = Units.inchesToMeters(49.2559230148);
 
     // Gear ratios from motor output to arm joint movement
     public static final double BOTTOM_GEAR_RATIO = 600;
