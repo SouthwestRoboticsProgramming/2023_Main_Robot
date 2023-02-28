b619119fcef2e1193e810eebf9ec48d7ba4db16b
mvog2501 <mvog2501@mpsedu.org>
2023-02-28 11:26:39 -0600

    [Robot] Fix robot not backing up on red alliance
    Don't know why this fixes it but it does

--- Robot/src/main/java/com/swrobotics/robot/commands/BalanceSequenceCommand.java
+++ Robot/src/main/java/com/swrobotics/robot/commands/BalanceSequenceCommand.java
@@ -2,7 +2,6 @@ package com.swrobotics.robot.commands;
 
 import com.swrobotics.lib.swerve.commands.DriveBlindCommand;
 import com.swrobotics.mathlib.Angle;
-import com.swrobotics.mathlib.CCWAngle;
 import com.swrobotics.robot.RobotContainer;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -12,9 +11,9 @@ public class BalanceSequenceCommand extends SequentialCommandGroup {
         Angle angle = Angle.ZERO;
 
         addCommands(
-            new StartBalanceCommand(robot, angle, -1.5, true).withTimeout(3),
+            new StartBalanceCommand(robot, angle, -1.5, false).withTimeout(3),
 
-            new DriveBlindCommand(robot, angle, -1.5, true).withTimeout(1), // Keep driving for 1 second
+            new DriveBlindCommand(robot, angle, -1.5, false).withTimeout(1), // Keep driving for 1 second
             new AutoBalanceCommand(robot)
         );
     }
