9f0c90a73386ed5f40d2125cfa6a5b344e0204be
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2023-03-18 14:05:53 -0500

    [Robot] Slight controls changes

--- Robot/src/main/java/com/swrobotics/robot/input/Input.java
+++ Robot/src/main/java/com/swrobotics/robot/input/Input.java
@@ -73,6 +73,7 @@ public final class Input extends SubsystemBase {
     private Angle snapAngle;
 
     private Translation2d prevArmTarget;
+    private boolean prevWasGrid;
 
     private GamePiece gamePiece;
 
@@ -91,6 +92,7 @@ public final class Input extends SubsystemBase {
         snapDriveCmd = new PathfindToPointCommand(robot, null);
         snapTurnCmd = new TurnToAngleCommand(robot, () -> snapAngle, false);
 
+        prevWasGrid = false;
         prevArmTarget = ArmPositions.DEFAULT.getTranslation();
         gamePiece = GamePiece.CUBE;
 
@@ -255,7 +257,7 @@ public final class Input extends SubsystemBase {
     }
 
     private static final double SNAP_DRIVE_TOL = 0.05;
-    private static final double SNAP_TURN_TOL = Math.toRadians(2);
+    private static final double SNAP_TURN_TOL = Math.toRadians(1);
 
     private void snapToPosition(Translation2d position) {
         snapDriveCmd.setGoal(new Vec2d(position.getX(), position.getY()));
@@ -306,10 +308,14 @@ public final class Input extends SubsystemBase {
         ));
 
         NTTranslation2d ntArmTarget = getArmTarget();
+        boolean isGrid = ntArmTarget != null
+            && ntArmTarget != ArmPositions.CUBE_PICKUP
+            && ntArmTarget != ArmPositions.CONE_PICKUP
+            && ntArmTarget != ArmPositions.DEFAULT;
         Translation2d armTarget = ntArmTarget == null ? robot.arm.getHomeTarget() : ntArmTarget.getTranslation();
 
         // If it is moving to a new target
-        if (!armTarget.equals(prevArmTarget)) {
+        if (!armTarget.equals(prevArmTarget) && (isGrid || prevWasGrid)) {
             armNudge = new Translation2d(0, 0);
 
             // Move to an intermediate position first
@@ -319,6 +325,7 @@ public final class Input extends SubsystemBase {
             }
         }
         prevArmTarget = armTarget;
+        prevWasGrid = isGrid;
 
         armTarget = armTarget.plus(armNudge);
         if (ntArmTarget != null && ntArmTarget != ArmPositions.DEFAULT) {
