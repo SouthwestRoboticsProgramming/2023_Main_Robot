302f738d37c37624f5b0f87fbbe3fa1e7c0fec84
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-03-29 13:21:42 -0500

    [Robot] Use motor lib for intake

--- Robot/src/main/java/com/swrobotics/robot/subsystems/intake/IntakeSubsystem.java
+++ Robot/src/main/java/com/swrobotics/robot/subsystems/intake/IntakeSubsystem.java
@@ -1,22 +1,22 @@
 package com.swrobotics.robot.subsystems.intake;
 
+import com.swrobotics.lib.motor.Motor;
+import com.swrobotics.lib.motor.rev.PWMSparkMaxMotor;
 import com.swrobotics.lib.net.NTDouble;
 import com.swrobotics.robot.RIOPorts;
 import com.swrobotics.lib.schedule.SwitchableSubsystemBase;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 public final class IntakeSubsystem extends SwitchableSubsystemBase {
-    private static final NTDouble L_INTAKE_SPEED = (NTDouble) new NTDouble("Intake/Current Speed", 0).setTemporary();
     private static final NTDouble CONE_HOLD = new NTDouble("Intake/Cone Hold", 0.1);
     private static final NTDouble CUBE_HOLD = new NTDouble("Intake/Cube Hold", -0.1);
 
-    private final PWMSparkMax motor;
+    private final Motor motor;
 
     private GamePiece expectedPiece;
     private boolean running;
 
     public IntakeSubsystem() {
-        motor = new PWMSparkMax(RIOPorts.INTAKE_PWM);
+        motor = new PWMSparkMaxMotor(RIOPorts.INTAKE_PWM);
 
         expectedPiece = GamePiece.CUBE;
         running = false;
@@ -32,30 +32,25 @@ public final class IntakeSubsystem extends SwitchableSubsystemBase {
 
     public void run() {
         if (isEnabled()) {
-            motor.set(expectedPiece.getIntakeOutput());
+            motor.setPercentOut(expectedPiece.getIntakeOutput());
             running = true;
         }
     }
 
     public void eject() {
         if (isEnabled()) {
-            motor.set(-expectedPiece.getOuttakeOutput());
+            motor.setPercentOut(-expectedPiece.getOuttakeOutput());
             running = true;
         }
     }
 
     public void stop() {
-        motor.set(expectedPiece == GamePiece.CONE ? (CONE_HOLD.get()) : CUBE_HOLD.get());
+        motor.setPercentOut(expectedPiece == GamePiece.CONE ? (CONE_HOLD.get()) : CUBE_HOLD.get());
         running = false;
     }
 
     @Override
     public void onDisable() {
-        motor.set(0);
-    }
-
-    @Override
-    public void periodic() {
-        L_INTAKE_SPEED.set(motor.get());
+        motor.setPercentOut(0);
     }
 }
