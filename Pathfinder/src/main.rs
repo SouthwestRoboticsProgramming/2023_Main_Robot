7ee20c7877f23f789c9e5b470a78340303e2f6b3
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-05-24 10:20:35 -0500

    [Pathfinding] Add dimensions from Monch CAD

--- Pathfinder/src/main.rs
+++ Pathfinder/src/main.rs
@@ -13,11 +13,13 @@ pub mod graph;
 pub mod theta_star;
 pub mod vectors;
 
-// Arm dimensions (FIXME: Wrong)
-pub const BOTTOM_LEN: f64 = 1.0; // Distance from bottom axis to midpoint axis
-pub const TOP_LEN: f64 = 0.8; // Distance from midpoint axis to wrist axis
-pub const INTAKE_RAD: f64 = 0.1; // Maximum distance of intake from wrist axis
-pub const MIDPOINT_RAD: f64 = 0.1; // Radius of middle joint pulley
+const IN_PER_M: f64 = 39.37;
+
+// Arm dimensions
+pub const BOTTOM_LEN: f64 = 35.0 / IN_PER_M; // Distance from bottom axis to midpoint axis
+pub const TOP_LEN: f64 = 29.95824774 / IN_PER_M; // Distance from midpoint axis to wrist axis
+pub const INTAKE_RAD: f64 = (6.49785148 + 1.125) / IN_PER_M; // Maximum distance of intake from wrist axis
+pub const MIDPOINT_RAD: f64 = 3.3 / IN_PER_M; // Radius of middle joint pulley
 
 pub const BOTTOM_GEAR_RATIO: f64 = 600.0;
 pub const TOP_GEAR_RATIO: f64 = 300.0;
@@ -26,12 +28,12 @@ pub const TOP_GEAR_RATIO: f64 = 300.0;
 const HEIGHT_LIMIT: f64 = 1.98;
 const EXTENSION_LIMIT: f64 = 1.22;
 
-// Limitations from robot (FIXME: Wrong)
-const FLOOR_HEIGHT: f64 = 0.2; // Vertical distance from floor to bottom joint pivot axis
-const BUMPER_THICKNESS: f64 = 0.05; // Thickness of bumper from frame edge to outer edge
-const FRAME_SIZE: f64 = 0.53; // Size of drive base without bumpers
+// Limitations from robot
+const FLOOR_HEIGHT: f64 = 9.62923832 / IN_PER_M; // Vertical distance from floor to bottom joint pivot axis
+const BUMPER_THICKNESS: f64 = 3.625 / IN_PER_M; // Thickness of bumper from frame edge to outer edge
+const FRAME_SIZE: f64 = 21.0 / IN_PER_M; // Size of drive base without bumpers
 const BASE_SIZE: f64 = FRAME_SIZE + 2.0 * BUMPER_THICKNESS;
-const BASE_HEIGHT: f64 = 0.1; // Distance from floor to top of drive base
+const BASE_HEIGHT: f64 = 4.7522244 / IN_PER_M; // Distance from floor to top of drive base
 
 // TODO: Grid collision rectangles
 
