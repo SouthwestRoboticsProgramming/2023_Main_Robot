0743091b7919c58ad172b07805cadd8cf3c0e5cd
mvog2501 <mvog2501@mpsedu.org>
2023-05-24 09:42:04 -0500

    Add wrist visualizer continued

--- Robot/src/main/java/com/swrobotics/robot/subsystems/arm/ArmVisualizer.java
+++ Robot/src/main/java/com/swrobotics/robot/subsystems/arm/ArmVisualizer.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj.util.Color8Bit;
 
 public final class ArmVisualizer {
-    private final MechanismLigament2d bottom, top;
+    private final MechanismLigament2d bottom, top, wrist;
 
     public ArmVisualizer(
             double x,
@@ -18,20 +18,21 @@ public final class ArmVisualizer {
             Mechanism2d visualization,
             String name,
             Color bottomColor,
-            Color topColor) {
+            Color topColor,
+            Color wristColor) {
         MechanismRoot2d root = visualization.getRoot(name, x, y);
-        bottom =
-                root.append(
-                        new MechanismLigament2d(
-                                "Bottom Arm",
-                                ArmConstants.BOTTOM_LENGTH,
-                                0,
-                                6,
-                                new Color8Bit(bottomColor)));
-        top =
-                bottom.append(
-                        new MechanismLigament2d(
-                                "Top Arm", ArmConstants.TOP_LENGTH, 0, 6, new Color8Bit(topColor)));
+        bottom = root.append(
+                new MechanismLigament2d(
+                        "Bottom Arm",
+                        ArmConstants.BOTTOM_LENGTH,
+                        0,
+                        6,
+                        new Color8Bit(bottomColor)));
+        top = bottom.append(
+                new MechanismLigament2d(
+                        "Top Arm", ArmConstants.TOP_LENGTH, 0, 6, new Color8Bit(topColor)));
+        wrist = top.append(
+                new MechanismLigament2d("Wrist", ArmConstants.WRIST_LENGTH / 2, 70, 6, new Color8Bit(wristColor)));
     }
 
     public void setPose(ArmPose pose) {
@@ -39,5 +40,7 @@ public final class ArmVisualizer {
 
         // Visualization is relative, pose is absolute
         top.setAngle(Math.toDegrees(pose.topAngle - pose.bottomAngle));
+
+        wrist.setAngle(pose.wristAngle);
     }
 }
