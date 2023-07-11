593deb355602b2d35a7094f0592315b771c60566
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-07-11 15:14:20 -0500

    [RobotLib] Add pigeon 1 gyroscope

--- RobotLib/src/main/java/com/swrobotics/lib/gyro/PigeonGyroscope.java
+++ RobotLib/src/main/java/com/swrobotics/lib/gyro/PigeonGyroscope.java
@@ -1,15 +1,16 @@
 package com.swrobotics.lib.gyro;
 
 import com.ctre.phoenix.sensors.Pigeon2;
+import com.ctre.phoenix.sensors.PigeonIMU;
 import com.swrobotics.mathlib.Angle;
 import com.swrobotics.mathlib.CCWAngle;
 import com.swrobotics.mathlib.Vec3d;
 
 /**
- * Gyroscope implementation for the Pigeon2 gyroscope from CTRE.
+ * Gyroscope implementation for the Pigeon IMU gyroscope from CTRE.
  */
 public final class PigeonGyroscope extends Gyroscope {
-    private final Pigeon2 pigeon;
+    private final PigeonIMU pigeon;
 
     /**
      * Creates a new instance of the Pigeon2 gyroscope with a specific CAN
@@ -18,18 +19,7 @@ public final class PigeonGyroscope extends Gyroscope {
      * @param canId CAN ID of the Pigeon2
      */
     public PigeonGyroscope(int canId) {
-        this(canId, "");
-    }
-
-    /**
-     * Creates a new instance of the Pigeon2 gyroscope with a specified CAN ID
-     * on a specified CAN bus.
-     *
-     * @param canId CAN ID of the Pigeon2 on the bus
-     * @param canBus name of the CAN bus the Pigeon2 is connected to
-     */
-    public PigeonGyroscope(int canId, String canBus) {
-        pigeon = new Pigeon2(canId, canBus);
+        pigeon = new PigeonIMU(canId);
     }
 
     @Override
@@ -42,12 +32,13 @@ public final class PigeonGyroscope extends Gyroscope {
         return CCWAngle.deg(pigeon.getYaw());
     }
 
-    /**
-     * Gets the vector aligned with gravity (i.e. pointing straight down).
-     */
-    public Vec3d getGravityVector() {
-        double[] xyz = new double[3];
-        pigeon.getGravityVector(xyz);
-        return new Vec3d(xyz[0], xyz[1], xyz[2]).normalize();
-    }
+    // TODO: make it work
+//    /**
+//     * Gets the vector aligned with gravity (i.e. pointing straight down).
+//     */
+//    public Vec3d getGravityVector() {
+//        double[] xyz = new double[3];
+//        pigeon.get(xyz);
+//        return new Vec3d(xyz[0], xyz[1], xyz[2]).normalize();
+//    }
 }
