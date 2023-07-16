f6f7c63ac6f63cbf237a870ff9915d0cffeca422
rmheuer <63077980+rmheuer@users.noreply.github.com>
2023-07-15 23:06:37 -0500

    [RobotLib] Fix position and velocity directions for Spark MAX

--- RobotLib/src/main/java/com/swrobotics/lib/motor/rev/SparkMaxMotor.java
+++ RobotLib/src/main/java/com/swrobotics/lib/motor/rev/SparkMaxMotor.java
@@ -76,13 +76,13 @@ public abstract class SparkMaxMotor implements FeedbackMotor {
 
     @Override
     public void setPosition(Angle position) {
-        pid.setReference(position.cw().rot(), CANSparkMax.ControlType.kPosition);
+        pid.setReference(position.ccw().rot(), CANSparkMax.ControlType.kPosition);
     }
 
     @Override
     public void setVelocity(Angle velocity) {
         pid.setReference(
-                Units.radiansPerSecondToRotationsPerMinute(velocity.cw().rad()),
+                Units.radiansPerSecondToRotationsPerMinute(velocity.ccw().rad()),
                 CANSparkMax.ControlType.kVelocity);
     }
 
